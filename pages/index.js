90bbdef4fbcc8bfd6628bb478ff252c1c4a7e0d7
DarrenKwonDev <anatomy1545@gmail.com>
2020-09-09 16:27:33 +0900

    dynamic route

--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,40 @@
 import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
+import Link from "next/link";
 
-export default function Home() {
+import Layout, { siteTitle } from "../components/layout";
+import { getSortedPostsData } from "../lib/posts";
+import utilStyles from "../styles/utils.module.css";
+import Date from "../components/date";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>
-          온라인 한국어 교육 플랫폼 EduPopKorn을 창업했으며 기술 부문을 담당하고
-          있습니다. 소규모 스타트업을 운영하기 위해 서비스 기획에서부터 클라우드
-          인프라 구축 및 배포까지 제너럴리스트가 되는 것이 목표입니다. 현재
-          Next+Node(웹), Flutter(모바일)를 주로 다루고 있으며 NLP에도 관심이
-          있는 편입니다.
-        </p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+      <Head>…</Head>
+      <section className={utilStyles.headingMd}>…</section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href="/posts/[id]" as={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
